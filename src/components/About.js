48fcdbb68560203494fa5d0eff49ec4fc9ed5595
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-07-14 00:06:39 +0300

    commit tree 9e807ede1fe14ded7fedcc919dc7834f9b67db57

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,13 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id="about">
+  <h2>About Me</h2>
+  <p>A lot about me is that which is diplayed in the element below.</p>
+  <img src={image} alt='I made this'/>
+  </div>
+  );
 }
 
 export default About;
